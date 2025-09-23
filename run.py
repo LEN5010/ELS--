1df1e52468371f91f3c0aec02b84e684c3d207b6
LEN5010 <1649211052@qq.com>
2025-09-23 10:54:50 +0800

    更新

--- run.py
+++ run.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
     port = int(os.getenv('FLASK_PORT', 5000))
     debug = os.getenv('FLASK_DEBUG', 'True').lower() == 'true'
     
-    app.run(host=host, port=5001, debug=debug)
+    app.run(host=host, port=port, debug=debug)
